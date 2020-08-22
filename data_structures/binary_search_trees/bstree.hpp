93a73fc764fbd449c015c356e1367e0a2e764aed
Mihir Singh <mihirs16@gmail.com>
2020-08-22 16:35:53 +0530

    BST init

---
+++ data_structures/binary_search_trees/bstree.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+struct node {
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+void insertElementBST (struct node *int val)
+
+// struct node *arrToBST (int arr[]) {
+    
+// }
